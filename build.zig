a7d16ba8f6076a2cd03208ed3e86892ab0d71913
keltonhalbert <kthalbert@wisc.edu>
2025-04-20 12:14:47 -0500

    Still learning this module stuff

--- build.zig
+++ build.zig
@@ -5,11 +5,20 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zfp",
+    const zfp_mod = b.createModule(.{
+        // `root_source_file` is the Zig "entry point" of the module. If a module
+        // only contains e.g. external object files, you can make this `null`.
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/zfp.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "zfp",
+        .root_module = zfp_mod,
+        .root_source_file = b.path("src/zfp.zig"),
     });
 
     lib.linkLibC();
